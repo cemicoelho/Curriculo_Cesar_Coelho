426cb5df0cf2431054272f97723ee8039290ccc9
cemicoelho <97065981+cemicoelho@users.noreply.github.com>
2023-08-27 19:04:45 +0000

    final

--- teste.js
+++
@@ -1,9 +0,0 @@
-//POP up de boas vindas
-$(document).ready(function () {
-    $("#popup").hide().fadeIn(1000);
-
-    $("#close").on("click", function (e) {
-        e.preventDefault();
-        $("#popup").fadeOut(1000);
-    });
-});
