c1226bd310b371871ecc34e17a08539f36ecba99
cemicoelho <97065981+cemicoelho@users.noreply.github.com>
2023-09-01 21:21:17 +0000

    limpeza_final

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     setTimeout(
-        function open(event){
+        function open(event) {
             document.querySelector(".popup").style.display = "block";
         },
         1000
     )
 });
-document.querySelector("#close").addEventListener("click", function(){
+document.querySelector("#close").addEventListener("click", function () {
     document.querySelector(".popup").style.display = "none";
-});
-
-//POP up de boas vindas
-$(document).ready(function () {
-    $("#popup").hide().fadeIn(1000);
-
-    $("#close").on("click", function (e) {
-        e.preventDefault();
-        $("#popup").fadeOut(1000);
-    });
 });
